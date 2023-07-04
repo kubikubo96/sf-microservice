d585007a6234a6cea15d2861a41be0aea5f4aec6
tiennt171 <tiennt171@ghtk.co>
2023-07-05 00:01:39 +0700

    update

--- micro-b/app/Helpers/RpcServer.php
+++ micro-b/app/Helpers/RpcServer.php
@@ -14,20 +14,25 @@ class RpcServer
 {
     private $connection;
 
-    public function __construct(){
-        $host = config('rabbitmq.connection.host');
-        $port = config('rabbitmq.connection.port');
-        $user = config('rabbitmq.connection.user');
-        $password = config('rabbitmq.connection.password');
-        $vhost = config('rabbitmq.connection.vhost');
-
-        $this->connection = new AMQPStreamConnection($host, $port, $user, $password, $vhost);
+    public function __construct()
+    {
+        $this->connection = new AMQPStreamConnection(
+            config('rabbitmq.connection.host'),
+            config('rabbitmq.connection.port'),
+            config('rabbitmq.connection.user'),
+            config('rabbitmq.connection.password')
+        );
     }
 
-    public function handle($queue, $exchange, $callback){
+    public function handle($queue, $exchange, $callback)
+    {
         $channel = $this->connection->channel();
 
         $channel->queue_declare($queue, false, true, false, false);
+
+        $channel->exchange_declare($exchange, AMQPExchangeType::DIRECT, false, true, false);
+
+        $channel->queue_bind($queue, $exchange);
 
         $channel->basic_qos(null, 1, null);
         $channel->basic_consume($queue, '', false, false, false, false, $callback);
