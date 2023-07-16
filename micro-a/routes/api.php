355ffca28b22ac09b26dcd5194f28c8c65a2e9f3
tiennt171 <tiennt171@ghtk.co>
2023-07-16 11:27:11 +0700

    update

--- micro-a/routes/api.php
+++ micro-a/routes/api.php
@@ -8,5 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('micro-a/send', [DemoController::class, 'send']);
 Route::get('micro-a/sendNotify', [DemoController::class, 'sendNotify']);
 
-Route::get('micro-a/createOrderV1', [OrderController::class, 'createOrderV1']);
-Route::get('micro-a/createOrderV2', [OrderController::class, 'createOrderV2']);
+Route::post('micro-a/createOrderV1', [OrderController::class, 'createOrderV1']);
+Route::post('micro-a/createOrderV2', [OrderController::class, 'createOrderV2']);
